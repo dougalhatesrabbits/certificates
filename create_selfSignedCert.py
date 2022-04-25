2bcc1b7770ad92723c187fb104cd76aa9debf2be
david <david.brooke14@icloud.com>
2022-04-25 21:32:38 +0100

    0.6 updated subprocess to Popen to capture return codes
    Updated Logging

--- create_selfSignedCert.py
+++ create_selfSignedCert.py
@@ -1,18 +1,47 @@
-import helper
+import time
+
+from helper import run, logger
+from subprocess import CalledProcessError
 import os
 
+# Working area
 projectLocation = os.getcwd()
 baseTlsLocation = os.path.join(projectLocation, "tls")
+opensslConf = os.path.join(baseTlsLocation, "openssl.cnf")
+
+# Self-Signed/CA
 privateFolder = os.path.join(baseTlsLocation, "private")
 certsFolder = os.path.join(baseTlsLocation, "certs")
-encPasswordFile = os.path.join(baseTlsLocation, "mypass.enc")
-opensslConf = os.path.join(baseTlsLocation, "openssl.cnf")
-privateKey = os.path.join(certsFolder, "server.key")
+caKey = os.path.join(privateFolder, "ec-cakey.pem")
+caCertificate = os.path.join(certsFolder, "ec-cacert.pem")
+ca_opensslConf = os.path.join(baseTlsLocation, "ca_cert.cnf")
+selfKey = os.path.join(privateFolder, "server.key")
 selfCertificate = os.path.join(certsFolder, "server.crt")
 selfCSR = os.path.join(certsFolder, "server.csr")
+self_opensslConf = os.path.join(baseTlsLocation, "self_signed_certificate.cnf")
+serialFile = os.path.join(baseTlsLocation, "serial")
+indexFile = os.path.join(baseTlsLocation, "index.txt")
+
+# Server
+serverFolder = os.path.join(baseTlsLocation, "server_certs")
+serverKey = os.path.join(privateFolder, "server.key")
+serverCSR = os.path.join(serverFolder, "server.csr")
+serverCert = os.path.join(serverFolder, "server.crt")
+server_opensslConf = os.path.join(baseTlsLocation, "server_cert.cnf")
+
+# Client
+clientFolder = os.path.join(baseTlsLocation, "client_certs")
+clientKey = os.path.join(privateFolder, "client.key")
+clientCSR = os.path.join(clientFolder, "client.csr")
+clientCert = os.path.join(clientFolder, "client.crt")
+client_opensslConf = os.path.join(baseTlsLocation, "client_cert.cnf")
+
+# Private key password file (option)
+encPasswordFile = os.path.join(privateFolder, "mypass.enc")
 
 
 def generate_private_key(pwd, key):
+    logger.debug("*** generate_private_key ***")
     command = 'openssl genrsa \
                -passout file:mypass.enc \
                -out server.key 4096'.split()
@@ -24,16 +53,19 @@ def generate_private_key(pwd, key):
     command.pop(5)
     command.insert(5, key)
     try:
-        print('command in list format:', command)
-        helper.run(command)
+        logger.debug(("Command executed:", ' '.join(command)))
+        rc = run(command)
+        logger.debug("*** generate_private_key *** return code: %s", rc)
+        time.sleep(1)
         verify_private_key(key, pwd)
     except OSError as error:
-        helper.logger.error(error)
-    except helper.subprocess.CalledProcessError as error:
-        helper.logger.error(error)
+        logger.error(error)
+    except CalledProcessError as error:
+        logger.error(error)
 
 
 def generate_csr(key, csr, pwd, cfg):
+    logger.debug("*** generate_csr ***")
     command = 'openssl req \
                -new \
                -key server.key \
@@ -50,16 +82,19 @@ def generate_csr(key, csr, pwd, cfg):
     command.pop(10)
     command.insert(10, cfg)
     try:
-        print('command in list format:', command)
-        helper.run(command)
+        logger.debug(("Command executed:", ' '.join(command)))
+        rc = run(command)
+        logger.debug("*** generate_csr *** return code: %s", rc)
+        time.sleep(1)
         verify_csr(csr)
     except OSError as error:
-        helper.logger.error(error)
-    except helper.subprocess.CalledProcessError as error:
-        helper.logger.error(error)
+        logger.error(error)
+    except CalledProcessError as error:
+        logger.error(error)
 
 
 def generate_x509_cert(csr, key, crt, pwd):
+    logger.debug("*** generate_x509_cert ***")
     command = 'openssl x509 \
                -req \
                -days 365 \
@@ -77,16 +112,19 @@ def generate_x509_cert(csr, key, crt, pwd):
     command.pop(12)
     command.insert(12, arg)
     try:
-        print('command in list format:', command)
-        helper.run(command)
+        logger.debug(("Command executed:", ' '.join(command)))
+        rc = run(command)
+        logger.debug("*** verify_private_key *** return code: %s", rc)
+        time.sleep(1)
         verify_self_signed_cert(crt)
     except OSError as error:
-        helper.logger.error(error)
-    except helper.subprocess.CalledProcessError as error:
-        helper.logger.error(error)
+        logger.error(error)
+    except CalledProcessError as error:
+        logger.error(error)
 
 
 def verify_private_key(key, pwd):
+    logger.debug("*** verify_private_key ***")
     command = 'openssl rsa \
                -noout \
                -text \
@@ -98,15 +136,17 @@ def verify_private_key(key, pwd):
     command.pop(7)
     command.insert(7, arg)
     try:
-        print('command in list format:', command)
-        helper.run(command)
+        logger.debug(("Command executed:", ' '.join(command)))
+        rc = run(command)
+        logger.debug("*** verify_private_key *** return code: %s", rc)
     except OSError as error:
-        helper.logger.error(error)
-    except helper.subprocess.CalledProcessError as error:
-        helper.logger.error(error)
+        logger.error(error)
+    except CalledProcessError as error:
+        logger.error(error)
 
 
 def verify_csr(csr):
+    logger.debug("*** verify_csr ***")
     command = 'openssl req \
                -noout \
                -text \
@@ -114,15 +154,17 @@ def verify_csr(csr):
     command.pop(5)
     command.insert(5, csr)
     try:
-        print('command in list format:', command)
-        helper.run(command)
+        logger.debug(("Command executed:", ' '.join(command)))
+        rc = run(command)
+        logger.debug("*** verify_csr *** return code: %s", rc)
     except OSError as error:
-        helper.logger.error(error)
-    except helper.subprocess.CalledProcessError as error:
-        helper.logger.error(error)
+        logger.error(error)
+    except CalledProcessError as error:
+        logger.error(error)
 
 
 def verify_self_signed_cert(crt):
+    logger.debug("*** verify_self_signed_cert ***")
     command = 'openssl x509 \
                -noout \
                -text \
@@ -130,18 +172,18 @@ def verify_self_signed_cert(crt):
     command.pop(5)
     command.insert(5, crt)
     try:
-        print('command in list format:', command)
-        helper.run(command)
+        logger.debug(("Command executed:", ' '.join(command)))
+        run(command)
     except OSError as error:
-        helper.logger.error(error)
-    except helper.subprocess.CalledProcessError as error:
-        helper.logger.error(error)
+        logger.error(error)
+    except CalledProcessError as error:
+        logger.error(error)
 
 
 if __name__ == '__main__':
-    generate_private_key(encPasswordFile, privateKey)
-    generate_csr(privateKey, selfCSR, encPasswordFile, opensslConf)
-    generate_x509_cert(selfCSR, privateKey, selfCertificate, encPasswordFile)
+    generate_private_key(encPasswordFile, selfKey)
+    generate_csr(selfKey, selfCSR, encPasswordFile, self_opensslConf)
+    generate_x509_cert(selfCSR, caKey, selfCertificate, encPasswordFile)
 
 
 
--- helper.py
+++ helper.py
@@ -3,9 +3,13 @@ import logging
 import subprocess
 import sys
 
-projectLocation = os.getcwd()
+TODO: "Encrypt log file possibly. Private keys are password protected with encrypted password file though. " \
+      "Only relevant for non password protected key option!"
 
-logfile = os.path.join(projectLocation, 'debug.log')
+projectLocation = os.getcwd()
+baseTlsLocation = os.path.join(projectLocation, "tls")
+privateFolder = os.path.join(baseTlsLocation, "private")
+logfile = os.path.join(privateFolder, 'debug.log')
 log_format = (
     '[%(asctime)s] %(levelname)-8s %(name)-12s %(message)s')
 
@@ -20,13 +24,44 @@ logging.basicConfig(
 logger = logging.getLogger("cert_logger")
 
 
+# https://docs.python.org/3.8/library/subprocess.html#
 def run(cmd):
+    rc = 0
+    '''
     sp = subprocess.run(cmd,
+                        # Dont like 'shell=True' for security reasons of unsanitised input
+                        # https://docs.python.org/2/library/subprocess.html#frequently-used-arguments
                         shell=False,
                         check=True,
                         capture_output=True,
                         text=True)
-
-    print("stdout: ", sp.stdout)
-    print("stderr: ", sp.stderr)
+    #print("stdout: ", sp.stdout)
+    #if sp.stderr != "":
+        #print("stderr: ", sp.stderr)
     logger.error(sp.stderr)
+    logger.debug(sp.stdout)
+    #print("stderr: ", sp.stderr)
+    '''
+
+    try:
+        sp = subprocess.Popen(cmd,
+                              shell=False,
+                              stdin=subprocess.PIPE,
+                              stdout=subprocess.PIPE,
+                              stderr=subprocess.PIPE,
+                              text=True)
+        output, error = sp.communicate(timeout=10)
+        rc = sp.wait()
+        if output:
+            logger.debug(output)
+        if error:
+            logger.error(error)
+    except subprocess.CalledProcessError as e:
+        logger.error(e.output)
+        logger.error("OpenSSL Return code: %s", e.returncode)
+    except subprocess.TimeoutExpired as e:
+        logger.error(e.output)
+
+    return rc
+
+
